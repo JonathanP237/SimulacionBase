4f95c2a9a5c3205794ab6734af782b03984eee56
Jonathan <japuentess@udistrital.edu.co>
2024-07-15 19:28:06 -0500

    se revierten cambios

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import "./globals.css";
 import Head from "next/head";
 import React from "react"; // Add this line
 const inter = Inter({ subsets: ["latin"] });
